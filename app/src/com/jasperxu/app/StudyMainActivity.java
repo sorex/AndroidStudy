c28b06b2022ad3e9b684f8ef01c48f0dd43f3d2c
sorex <sorex1984@gmail.com>
2014-04-28 22:29:11 +0800

    添加"用代码添加控件"的示例

--- app/src/com/jasperxu/app/StudyMainActivity.java
+++ app/src/com/jasperxu/app/StudyMainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import com.jasperxu.app.study.AddViewByCodeActivity;
 
 /**
  * Created by Jasper on 2014/4/28.
@@ -26,5 +27,14 @@ public class StudyMainActivity extends Activity {
     public void CloseHandler(View view)
     {
         finish();
+    }
+
+    public void AddViewByCodeHandler(View view)
+    {
+        Intent intent = new Intent();
+        intent.setClass(StudyMainActivity.this, AddViewByCodeActivity.class);
+        startActivity(intent);
+        //如果不关闭当前的会出现好多个页面
+        StudyMainActivity.this.finish();
     }
 }
--- app/src/com/jasperxu/app/study/AddViewByCodeActivity.java
+++ app/src/com/jasperxu/app/study/AddViewByCodeActivity.java
@@ -1,8 +1,18 @@
 package com.jasperxu.app.study;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.util.Linkify;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import com.jasperxu.app.MainActivity;
 import com.jasperxu.app.R;
+import com.jasperxu.app.StudyMainActivity;
 
 /**
  * Created by Jasper on 2014/4/28.
@@ -11,5 +21,45 @@ public class AddViewByCodeActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.study_add_view_by_code);
+    }
+
+
+    public void GoBackHandler(View view) {
+        Intent intent = new Intent();
+        intent.setClass(AddViewByCodeActivity.this, StudyMainActivity.class);
+        startActivity(intent);
+        //如果不关闭当前的会出现好多个页面
+        AddViewByCodeActivity.this.finish();
+    }
+
+    public void RunHandler(View view) {
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
+        for (int i = 0; i < 20; i++) {
+            final int final_i = i;
+            TextView tv = new TextView(this);
+            tv.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));//设置TextView的布局
+            //tv1.setPadding(50, 100, 0, 0);//设置边距
+            tv.setText(i + ".这是一个动态添加的内容！");
+            tv.setAutoLinkMask(Linkify.ALL);
+            tv.setTextSize(20);
+            tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    new AlertDialog.Builder(AddViewByCodeActivity.this)
+                            .setTitle("提示标题")
+                            .setMessage("这是提示内容" + final_i)
+                            .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.cancel();
+                                }
+                            })
+                            .show();
+                }
+            });
+            layout.addView(tv);//将TextView 添加到子View 中
+        }
     }
 }
